4e20cd544be16c5dee9488283c4aba4692ac55fb
Juan Cruz Etchart <etchart.juancruz@gmail.com>
2023-04-30 19:24:40 -0300

    Modelos de géneros y producciones

---
+++ src/database/models/Genre.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, dataTypes) => {
+  let alias = "Genre";
+
+  let cols = {
+    id: {
+      type: dataTypes.INTEGER(11),
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+      unique: { args: true, msg: "El género ya fue creado!" },
+    },
+    image: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+    },
+  };
+
+  let config = {
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    tableName: "genres",
+  };
+  const Genre = sequelize.define(alias, cols, config);
+
+  Genre.associate = (models) => {
+    Genre.belongsToMany(models.Production, {
+      as: "productions",
+      through: "production_genre",
+      foreignKey: "genre_id",
+      otherKey: "production_id",
+      timestamps: false,
+    });
+  };
+};
---
+++ src/database/models/Production.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, dataTypes) => {
+  let alias = "Production";
+
+  let cols = {
+    id: {
+      type: dataTypes.INTEGER(11),
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+      unique: { args: true, msg: "La producción ya fue creada!" },
+    },
+    image: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+    },
+  };
+
+  let config = {
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    tableName: "genres",
+  };
+  const Production = sequelize.define(alias, cols, config);
+
+  Production.associate = (models) => {
+    Production.belongsToMany(models.Production, {
+      as: "genres",
+      through: "production_genre",
+      foreignKey: "production_id",
+      otherKey: "genre_id",
+      timestamps: false,
+    });
+  };
+};
